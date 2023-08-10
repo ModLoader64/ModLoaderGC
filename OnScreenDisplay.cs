fc90f6f6ef7fc8dc253b5c2f59fd44f6d6faa13c
maraon64 <17965220+maraon64@users.noreply.github.com>
2023-08-10 15:28:11 +0200

    add imgui support

---
+++ OnScreenDisplay.cs
@@ -0,0 +1,24 @@
+﻿using static DolphinEmu.pinvoke.OnScreenDisplayImports;
+
+namespace DolphinEmu;
+
+public static class OnScreenDisplay
+{
+    public enum Color : uint
+    {
+        Cyan = 0xFF00FFFF,
+        Green = 0xFF00FF00,
+        Red = 0xFFFF0000,
+        Yellow = 0xFFFFFF30
+    }
+
+    public enum Duration : uint
+    {
+        Short = 2000,
+        Normal = 5000,
+        VeryLong = 10000
+    }
+
+    public static void AddMessage(string message, Duration ms, Color argb)
+        => on_screen_display_add_message(message, (uint)ms, (uint)argb);
+}
---
+++ pinvoke/OnScreenUIImports.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DolphinEmu.pinvoke;
+
+internal static partial class OnScreenUIImports
+{
+    [LibraryImport(DolphinLibrary.Name)]
+    public static partial void on_screen_ui_set_imgui_init_callback(
+        OnScreenUI.ImGuiHookCallbackFunc callback);
+
+    [LibraryImport(DolphinLibrary.Name)]
+    public static partial void on_screen_ui_set_imgui_render_callback(
+        OnScreenUI.ImGuiHookCallbackFunc callback);
+}
