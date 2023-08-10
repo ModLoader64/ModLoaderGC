32a32d10d7284fd66b9ac2c182de8e7bfec644b2
maraon64 <17965220+maraon64@users.noreply.github.com>
2023-08-10 16:42:31 +0200

    add save state api

---
+++ State.cs
@@ -0,0 +1,51 @@
+﻿using static DolphinEmu.pinvoke.StateImports;
+
+namespace DolphinEmu;
+
+public static class State
+{
+    public static void Save(int slot, bool wait = false)
+        => state_save(slot, wait);
+
+    public static void Load(int slot)
+        => state_load(slot);
+
+    public static void SaveAs(string filename, bool wait = false)
+        => state_save_as(filename, wait);
+
+    public static void LoadAs(string filename)
+        => state_load_as(filename);
+
+    public sealed class Buffer : IDisposable
+    {
+        public IntPtr Data { get; }
+        public ulong Size { get; }
+
+        public Buffer(IntPtr data, ulong size)
+        {
+            Data = data;
+            Size = size;
+        }
+
+        private void Free()
+            => state_free_buffer_data(Data);
+
+        public void Dispose()
+        {
+            Free();
+            GC.SuppressFinalize(this);
+        }
+
+        ~Buffer()
+            => Free();
+    }
+
+    public static Buffer SaveToBuffer()
+    {
+        state_save_to_buffer(out var data, out var size);
+        return new Buffer(data, size);
+    }
+
+    public static void LoadFromBuffer(Buffer buffer)
+        => state_load_from_buffer(buffer.Data, buffer.Size);
+}
---
+++ pinvoke/StateImports.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DolphinEmu.pinvoke;
+
+internal static partial class StateImports
+{
+    [LibraryImport(DolphinLibrary.Name)]
+    public static partial void state_save(
+        int slot,
+        [MarshalAs(UnmanagedType.Bool)] bool wait);
+
+    [LibraryImport(DolphinLibrary.Name)]
+    public static partial void state_load(
+        int slot);
+
+    [LibraryImport(DolphinLibrary.Name, StringMarshalling = StringMarshalling.Utf8)]
+    public static partial void state_save_as(
+        string filename,
+        [MarshalAs(UnmanagedType.Bool)] bool wait);
+
+    [LibraryImport(DolphinLibrary.Name, StringMarshalling = StringMarshalling.Utf8)]
+    public static partial void state_load_as(
+        string filename);
+
+    [LibraryImport(DolphinLibrary.Name)]
+    public static partial void state_save_to_buffer(
+        out IntPtr data,
+        out ulong size);
+
+    [LibraryImport(DolphinLibrary.Name)]
+    public static partial void state_load_from_buffer(
+        IntPtr data,
+        ulong size);
+
+    [LibraryImport(DolphinLibrary.Name)]
+    public static partial void state_free_buffer_data(
+        IntPtr data);
+}
