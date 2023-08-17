3386fbf124a030cad3ae4e5d2e8f18f873c4ae29
maraon64 <17965220+maraon64@users.noreply.github.com>
2023-08-17 23:43:28 +0200

    merge to osd

---
+++ Osd.cs
@@ -0,0 +1,32 @@
+﻿using static DolphinEmu.pinvoke.OsdImports;
+
+namespace DolphinEmu;
+
+public static class Osd
+{
+    public enum Color : uint
+    {
+        Cyan = 0xFF00FFFF,
+        Green = 0xFF00FF00,
+        Red = 0xFFFF0000,
+        Yellow = 0xFFFFFF30
+    }
+
+    public enum Duration : uint
+    {
+        Short = 2000,
+        Normal = 5000,
+        VeryLong = 10000
+    }
+
+    public static void AddMessage(string message, Duration ms, Color argb)
+        => osd_add_message(message, (uint)ms, (uint)argb);
+
+    public delegate void ImGuiCallbackFunc(IntPtr context);
+
+    public static void SetImGuiInitCallback(ImGuiCallbackFunc callback)
+        => osd_set_imgui_init_callback(callback);
+
+    public static void SetImGuiRenderCallback(ImGuiCallbackFunc callback)
+        => osd_set_imgui_render_callback(callback);
+}
---
+++ pinvoke/OsdImports.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DolphinEmu.pinvoke;
+
+internal static partial class OsdImports
+{
+    [LibraryImport(DolphinLibrary.Name, StringMarshalling = StringMarshalling.Utf8)]
+    public static partial void osd_add_message(
+        string message,
+        uint ms,
+        uint argb);
+
+    [LibraryImport(DolphinLibrary.Name)]
+    public static partial void osd_set_imgui_init_callback(
+        Osd.ImGuiCallbackFunc callback);
+
+    [LibraryImport(DolphinLibrary.Name)]
+    public static partial void osd_set_imgui_render_callback(
+        Osd.ImGuiCallbackFunc callback);
+}
